85c7ba75403e410c5ad013df979c87d917c6f175
Morgan Rector <mmrector@yahoo.com>
2022-11-12 13:59:36 -0800

    Cat rental migrations, model, and custom validations working

--- lots-of-cats/app/models/cat.rb
+++ lots-of-cats/app/models/cat.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  name        :string           not null
+#  color       :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "action_view"
 class Cat < ApplicationRecord
     ActionView::Helpers::DateHelper
@@ -16,4 +29,10 @@ class Cat < ApplicationRecord
     def age 
         Date.today.year - birth_date.to_date.year
     end
+
+    has_many :rental_requests,
+        primary_key: :id,
+        foreign_key: :cat_id,
+        class_name: :Cat,
+        dependent: :destroy
 end
