11002b2e9b6c16996c4c290c18d289b449a48cce
Morgan Rector <mmrector@yahoo.com>
2022-11-12 11:59:18 -0800

    Form added and data persisting to database

--- lots-of-cats/app/controllers/concerns/cats_controller.rb
+++ lots-of-cats/app/controllers/concerns/cats_controller.rb
@@ -1,35 +1,47 @@
 class CatsController < ApplicationController
     def index
-        cats = Cat.all
-        render json: cats
+        @cats = Cat.all
+        render :index
     end
 
     def show
-        cat = Cat.find(params[:id])
-        render json: cat
+        @cat = Cat.find(params[:id])
+
+        if @cat
+            render :show
+        else
+            redirect_to cats_url
+        end
     end
 
     def create
-        new_cat = Cats.new(cat_params)
-        if new_cat.save!
-            render json: new_cat
+         @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
         else
-            render json: new_cat.errors.full_messages, status: 422
+            render :form
         end
     end
 
-    def update
-        cat_to_edit = Cat.find_by(:id, params[:id])
+    def new
+        render :form
+    end
 
-        if cat_to_edit.update(cat_params)
-            render json: cat_to_edit
+    def update
+        @cat = Cat.find_by(:id, params[:id])
+
+        if @cat.update(cat_params)
+            render json: cat
         else
-            render json: cat_to_edit.errors.full_messages, status: 422
+            render json: @cat.errors.full_messages, status: 422
         end
+    end
+
+    def edit
     end
 
     private
     def cat_params
-        params.require(:cats).permit(:name, :birth_date, :color, :sex, :description)
+        params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
     end
 end
--- lots-of-cats/db/seeds.rb
+++ lots-of-cats/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+morgan = Cat.create!({birth_date: '1996/07/10', name: "morgan", sex: 'F', color: 'silver', description: 'cuddly and fun'})
+bacheeva = Cat.create!({birth_date: '1999/07/10', name: "bacheeva", sex: 'F', color: 'black', description: 'cuddly and fun'})
+shaye = Cat.create!({birth_date: '1999/07/10', name: "shaye", sex: 'F', color: 'silver', description: 'sassy and fun'})
